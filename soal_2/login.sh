9743e4e5e691a30826f6bebf4f47ed3ffd53ec7b
khumaidi kharis az-zacky <zackyskak5@gmail.com>
2025-03-21 18:52:00 +0700

    Update login.sh

--- soal_2/login.sh
+++ soal_2/login.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Path to the player database
 PLAYER_DB="./data/player.csv"
 
-# Input email
 echo "Enter your email:"
 read email
 
-# Input password
 echo "Enter password:"
 read -s password
 
-# Hash the password with sha256sum
 hashed_password=$(echo -n "$password" | sha256sum | awk '{print $1}')
 
-# Check if email and password match
 if grep -q "^$email,.*,$hashed_password$" "$PLAYER_DB"; then
     echo "Login successful!"
     exit 0
