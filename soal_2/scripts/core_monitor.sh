cb748de6090fbb24a7ab7ab1e6248e009ff43dae
khumaidi kharis az-zacky <zackyskak5@gmail.com>
2025-03-21 18:50:34 +0700

    Update core_monitor.sh

--- soal_2/scripts/core_monitor.sh
+++ soal_2/scripts/core_monitor.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Mendapatkan persentase penggunaan CPU
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 cpu_model=$(lscpu | grep "Model name" | sed 's/Model name:\s*//')
 
-# Format log untuk CPU
 log_message="[$(date '+%Y-%m-%d %H:%M:%S')] - Core Usage [$cpu_usage%] - Terminal Model [$cpu_model]"
 
-# Tulis log ke file core.log
 echo $log_message >> ./logs/core.log
