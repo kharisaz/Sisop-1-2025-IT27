22d66a94086b21efc16003970714036aafc1690a
khumaidi kharis az-zacky <zackyskak5@gmail.com>
2025-03-21 18:51:01 +0700

    Update frag_monitor.sh

--- soal_2/scripts/frag_monitor.sh
+++ soal_2/scripts/frag_monitor.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Mendapatkan persentase penggunaan RAM
 ram_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
 ram_total=$(free -m | grep Mem | awk '{print $2}')
 ram_available=$(free -m | grep Mem | awk '{print $7}')
 
-# Format log untuk RAM
 log_message="[$(date '+%Y-%m-%d %H:%M:%S')] - Fragment Usage [$ram_usage%] - Fragment Count [$ram_available MB] - Details [Total: $ram_total MB, Available: $ram_available MB]"
 
-# Tulis log ke file fragment.log
 echo $log_message >> ./logs/fragment.log
